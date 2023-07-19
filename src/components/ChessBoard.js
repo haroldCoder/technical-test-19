f6b9ea5889272bbef437a9ad9efde962cd8df114
haroldCoder <haroldc2005@gmail.com>
2023-07-19 09:28:42 -0500

    create two components

---
+++ src/components/ChessBoard.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ChessBoard() {
+  return (
+    <div>ChessBoard</div>
+  )
+}
---
+++ src/components/Squres.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Squres() {
+  return (
+    <div>Squres</div>
+  )
+}
