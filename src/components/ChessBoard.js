7e277dedeaab0cfbd6281eb1a2fe99099b9beb59
haroldCoder <haroldc2005@gmail.com>
2023-07-19 10:00:10 -0500

    chessboard and squres whith logic

--- src/components/ChessBoard.js
+++ src/components/ChessBoard.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Squres from './Squres'
 
 export default function ChessBoard() {
+    const [colorsquare, setColorSquare] = useState("white");
+
+    function getSquareColor(index) {
+        const row = Math.floor(index / 8);
+        const col = index % 8;
+      
+        if (row % 2 === 0) {
+          // Filas pares
+          return col % 2 === 0 ? "black" : "white";
+        } else {
+          // Filas impares
+          return col % 2 === 0 ? "white" : "black";
+        }
+      }
+
   return (
-    <div>ChessBoard</div>
+    <div className='grid grid-cols-8 w-[25%] border-2 border-[#222]'>
+        {
+            Array.from({length: 64}, (_, index)=>(
+                <Squres color={getSquareColor(index)} />
+            ))
+        }
+    </div>
   )
 }
--- src/components/Squres.js
+++ src/components/Squres.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
-export default function Squres() {
+export default function Squres({color}) {
+    console.log(color);
   return (
-    <div>Squres</div>
+    <div className={`w-[50px] h-[50px] m-0 p-0 border-[1px] border-gray-800 ${color == "black" ? "bg-black" : "bg-white"}`}>
+        
+    </div>
   )
 }
